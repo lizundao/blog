85f8efc3d8ecc11162a222e7f4838d981b831eaf
李遵道 <lizundao@lizundaodeMacBook-Pro.local>
2026-06-12 21:30:32 +0800

    add post

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,23 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lizundao.com',
   output: 'static',
+
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
     mdx(),
     sitemap(),
   ],
+
   markdown: {
     shikiConfig: { theme: 'github-dark' },
   },
+
+  adapter: cloudflare()
 });
